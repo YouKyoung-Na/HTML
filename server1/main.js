278b779efeed807db5f4f6b53ada92b0104e31d4
Student <dbrudskql823@naver.com>
2022-05-30 16:27:34 +0900

    2022.05.30

--- server1/main.js
+++ server1/main.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const htppscode=require('http-status-code');
 const server = http.createServer((req, res)=>{
-
-})
-
+    res.writeHead(httpcode.StatysCodes.Ok,{"Content-Type": "text/html"});
+    res.write("<h1>Welcome To Sample Node Web Server</h1>")
+    res.end();
+});
+server.listen(port, ()=>{console.log("server is runnig on port 3000")});
 //webserver 404 오류는 없다는 뜻
